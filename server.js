daf8f3966784406315631ea9152dffc387e0ea8b
Eduardo-Muller <eduardomuller0301@gmail.com>
2021-08-09 04:45:18 -0300

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 5000;
 const app = express()
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,"build")));
 app.get("/ping", function(req,res) {
