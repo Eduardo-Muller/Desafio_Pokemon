71fde8b6287cd9b05a01564e29844290fb2d814f
Eduardo-Muller <eduardomuller0301@gmail.com>
2021-08-08 22:58:23 -0300

    added routes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 //Component
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
@@ -20,7 +20,8 @@ import { PokeListComponent } from './poke-list/poke-list.component';
   PokeListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
